415c1886edd2b4c26d01cccf33a0c51b473cb7a4
wangyongyong <3524493614@qq.com>
2019-09-24 13:44:33 +0800

    升级AndroidX

--- android/src/main/java/com/jzxl/ocs_plugin/base/adapter/recyclerview/ViewHolder.java
+++ android/src/main/java/com/jzxl/ocs_plugin/base/adapter/recyclerview/ViewHolder.java
@@ -7,7 +7,6 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.support.v7.widget.RecyclerView;
 import android.text.SpannableString;
 import android.text.util.Linkify;
 import android.util.SparseArray;
@@ -24,6 +23,8 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class ViewHolder extends RecyclerView.ViewHolder {
